d8adec1135a765a638b4c4c027b19857b6f2e994
JDOdea <jondavidodea@gmail.com>
2025-03-04 21:32:48 -0600

    initialized led-matrix.h

---
+++ include/led-matrix.h
@@ -0,0 +1,12 @@
+// Control 16x32 RGB matrixes via GPIO. It allows daisy chaining
+// of these in a string, while also connacting a parallel string on newer
+// Raspberry Pis with more GPIO pins available.
+
+#ifndef RPI_RGBMATRIX_H
+#define RPI_RGBMATRIX_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+// end namespace rgb_matrix
+#endif // RPI_RGBMATRIX_H
